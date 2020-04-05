f5c92fe134ae0406f75985934493b794828ad51c
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2020-04-05 22:14:24 +0300

    Message above point w depth/time

--- app/static/d3.js
+++ app/static/d3.js
@@ -69,59 +69,134 @@ svg.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
 
+
+    svg.append("text")
+    .attr("transform", "translate(" + (width / 2) + " ," + (height + margin.bottom) + ")")
+    .style("text-anchor", "middle")
+    .style("fill", "olive")
+    .text("Runtime (min)");
+
 // 4. Call the y axis in a group tag
 svg.append("g")
     .attr("class", "y axis")
     .attr("class", "axisColor")
     .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
 
+
+    // Add the text label for the Y axis
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x",0 - (height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "start")
+        .style("fill", "olive")
+        .text("Depth (meters)");
+
+
 // 9. Append the path, bind the data, and call the line generator 
 svg.append("path")
     .datum(dataset) // 10. Binds data to the line 
     .attr("class", "line") // Assign a class for styling 
     .attr("d", line); // 11. Calls the line generator 
-/*
+
 // 12. Appends a circle for each datapoint 
-//svg.selectAll(".dot")
-//    .data(dataset)
- // .enter().append("circle") // Uses the enter().append() method
- //   .attr("class", "dot") // Assign a class for styling
- //   .attr("cx", function(d, i) { return xScale(i) })
- //   .attr("cy", function(d) { return yScale(d.y) })
- //   .attr("r", 5)
- //     .on("mouseover", function(a, b, c) { 
- // 			console.log(a) 
- //      this.attr('class', 'focus')
-//		})
-//      .on("mouseout", function() {  })
-//       .on("mousemove", mousemove);
+svg.selectAll(".dot")
+    .data(dataset)
+  .enter().append("circle") // Uses the enter().append() method
+    .attr("class", "dot") // Assign a class for styling
+    .attr("cx", function(d) { return xScale(d.time) })
+    .attr("cy", function(d) { return yScale(d.depth) })
+    .attr("r", 5);
+/* 
+      .on("mouseover", function() { 
+  			//console.log(a) 
+       this.attr('class', 'focus')
+		})
+      .on("mouseout", function() {  })
+       .on("mousemove", mousemove);
 
-//   var focus = svg.append("g")
-//       .attr("class", "focus")
-//       .style("display", "none");
+  var focus = svg.append("g")
+       .attr("class", "focus")
+       .style("display", "none");
 
-//   focus.append("circle")
-//       .attr("r", 4.5);
+   focus.append("circle")
+       .attr("r", 4.5);
 
-//   focus.append("text")
-//       .attr("x", 9)
-//       .attr("dy", ".35em");
+  focus.append("text")
+       .attr("x", 9)
+       .attr("dy", ".35em");
 
-//   svg.append("rect")
-//       .attr("class", "overlay")
-//       .attr("width", width)
-//       .attr("height", height)
-//       .on("mouseover", function() { focus.style("display", null); })
-//       .on("mouseout", function() { focus.style("display", "none"); })
-//       .on("mousemove", mousemove);
+   svg.append("rect")
+       .attr("class", "overlay")
+       .attr("width", width)
+       .attr("height", height)
+       .on("mouseover", function() { focus.style("display", null); })
+       .on("mouseout", function() { focus.style("display", "none"); })
+       .on("mousemove", mousemove);
   
-//   function mousemove() {
-//     var x0 = x.invert(d3.mouse(this)[0]),
-//         i = bisectDate(data, x0, 1),
-//         d0 = data[i - 1],
-//         d1 = data[i],
-//         d = x0 - d0.date > d1.date - x0 ? d1 : d0;
-//     focus.attr("transform", "translate(" + x(d.date) + "," + y(d.close) + ")");
-//     focus.select("text").text(d);
-//   }
+   function mousemove() {
+     var x0 = xScale.invert(d3.mouse(this)[0]),
+         i = bisectDate(data, x0, 1),
+         d0 = dataset[i - 1],
+         d1 = dataset[i],
+         d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+     focus.attr("transform", "translate(" + x(d.time) + "," + y(d.depth) + ")");
+     focus.select("text").text(d);
+   }
 */
+ // This allows to find the closest X index of the mouse:
+ var bisect = d3.bisector(function(d) { return d.time; }).left;
+
+ // Create the circle that travels along the curve of chart
+ var focus = svg
+   .append('g')
+   .append('circle')
+     .style("fill", "none")
+     .attr("stroke", "black")
+     .attr('r', 7)
+     .style("opacity", 0)
+
+ // Create the text that travels along the curve of chart
+ var focusText = svg
+   .append('g')
+   .append('text')
+     .style("opacity", 0)
+     .style('fill', 'olive')
+     .attr("text-anchor", "start")
+     .attr("alignment-baseline", "top")
+
+ // Create a rect on top of the svg area: this rectangle recovers mouse position
+ svg
+   .append('rect')
+   .style("fill", "none")
+   .style("pointer-events", "all")
+   .attr('width', width)
+   .attr('height', height)
+   .on('mouseover', mouseover)
+   .on('mousemove', mousemove)
+   .on('mouseout', mouseout);
+
+// What happens when the mouse move -> show the annotations at the right positions.
+function mouseover() {
+  focus.style("opacity", 1)
+  focusText.style("opacity",1)
+}
+
+function mousemove() {
+  // recover coordinate we need
+  var x0 = xScale.invert(d3.mouse(this)[0]);
+  var i = bisect(dataset, x0, 1);
+  selectedData = dataset[i]
+  focus
+    .attr("cx", xScale(selectedData.time))
+    .attr("cy", yScale(selectedData.depth))
+  focusText
+    .html(selectedData.depth + " m" + " - "  +  selectedData.time + "min(RT)")
+    .attr("x", xScale(selectedData.time)-80)
+    .attr("y", yScale(selectedData.depth)-20)
+  }
+function mouseout() {
+  focus.style("opacity", 0)
+  focusText.style("opacity", 0)
+}
