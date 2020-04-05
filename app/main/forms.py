1d74c766497eb67680a05970ef3eaedd430ea982
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2020-04-05 14:07:08 +0300

    Flask form/dive details

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField, SelectField
 from wtforms.validators import Required
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
+
+class DiveForm(FlaskForm):
+    depth = IntegerField('Depth', validators=[Required()])
+    #gas = IntegerField('Gas', validators=[Required()])
+    gas = SelectField(u'Gas Switch', choices=[(21, '21m - 50%'), (6, 'Oxygen'), (0, 'Surface')])
+    submit = SubmitField('Dive!')
--- app/main/views.py
+++ app/main/views.py
@@ -1,20 +1,18 @@
 from flask import render_template, session, redirect, url_for, flash, current_app
 from . import main
 from ..import db
-from .forms import NameForm
+from .forms import DiveForm
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-@main.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
 
 @main.route('/minimum-gas')
 def minimum_gas():
-    return render_template('min_gas.html')
+    form = DiveForm()
+    return render_template('min_gas.html', form=form)
 
 @main.route('/contactus')
 def contactus():
