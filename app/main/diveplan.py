d3e506b2279eb9dbcb970a52682d750c1caff9cf
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2020-04-12 16:45:50 +0300

    comment out try-outs

--- app/main/diveplan.py
+++ app/main/diveplan.py
@@ -10,9 +10,6 @@ def find_first_stop(depth=43):
 def min_gas_plan(start_depth, target_depth, solve_time):
     target_index = target_depth / 3
     first_stop, first_index = find_first_stop(start_depth)
-    #plan = [[start_depth, 1],
-    #        [first_stop, 1],
-    #]
     plan = [[start_depth, solve_time], 
             [first_stop, ((start_depth - first_stop ) // 9) + solve_time + 1]]
     if first_stop > target_depth:
@@ -24,12 +21,11 @@ def min_gas_plan(start_depth, target_depth, solve_time):
             previous_rt = plan[-1][1]
             plan.append([next_stop, previous_rt + 1])
     plan.append([plan[-1][0], plan[-1][1]+1]) #add 1 ' to gas switch depth for gas switch duration
-    print(plan)
+    #print(plan)
     return plan
 
 
 
 
 #find_first_stop()
-
-min_gas_plan(45, 21, 1)
+#min_gas_plan(45, 21, 1)
