065f539aaeb91223d5e0cec4868b0108211ffedf
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2020-08-30 22:19:56 +0300

    .env enviroment vars

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+from dotenv import load_dotenv
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
