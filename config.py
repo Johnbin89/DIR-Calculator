a82e82200da8d8505b0aa469500eb837c6859ba5
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2020-08-30 21:43:24 +0300

    secrets

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 's12567dg'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
@@ -12,16 +12,6 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-    ('mysql://***REMOVED***:***REMOVED***@***REMOVED***/***REMOVED***')
-
-    MYSQL_USER = '***REMOVED***'
-    MYSQL_PASSWORD = '***REMOVED***'
-    MYSQL_HOST = '***REMOVED***'
-    MYSQL_DB = '***REMOVED***'
-    MYSQL_CURSORCLASS = 'DictCursor'
-    '''
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
