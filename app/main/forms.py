cb5c611aa0f225ac8e03fe8a8503bea42c6a8d8a
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2023-10-04 13:31:44 +0300

    update packages/Flask-Script->Flask-CLI

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField, SelectField, HiddenField
-from wtforms.validators import Required
+from wtforms.validators import DataRequired
 
 class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[Required()])
+    name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 class DiveForm(FlaskForm):
-    depth = IntegerField('Depth', validators=[Required()])
-    solve = IntegerField('Solving time', validators=[Required()], default=1)
+    depth = IntegerField('Depth', validators=[DataRequired()])
+    solve = IntegerField('Solving time', validators=[DataRequired()], default=1)
     gas = SelectField(u'Gas Switch', choices=[('21', '21m - 50%'), ('6', 'Oxygen'), ('0', 'Surface')])
     submit = SubmitField('Dive!')
 
--- manage.py
+++ manage.py
@@ -1,28 +1,25 @@
 import os
 from app import create_app, db
 from app.models import *
-from flask_script import Manager, Shell
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-manager = Manager(app)
 migrate = Migrate(app, db)
 
+@app.shell_context_processor
 def make_shell_context():                                                  #integrade db with python shell
     #return dict(app=app, db=db, User=User, Role=Role)
     return dict(app=app, db=db)
 
-manager.add_command("shell", Shell(make_context=make_shell_context))   #integrade db with python shell
-manager.add_command('db', MigrateCommand)
 
-@manager.command
+@app.cli.command()
 def test():
     """Run the unit tests."""
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
-@manager.command
+@app.cli.command()
 def expired_links():
     import datetime
     """Check for expired shared links"""
@@ -32,6 +29,3 @@ def expired_links():
             db.session.delete(link)
         db.session.commit()
 
-
-if __name__ == "__main__":
-    manager.run()
