850eda1b1daa90b1842992294e3106b0908348f9
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2024-01-27 16:23:11 +0200

    use eventlet in gunicorn for flask-socketio

--- boot.sh
+++ boot.sh
@@ -3,4 +3,4 @@ source env/bin/activate
 flask db upgrade
 service cron start
 crontab explinks-crontab
-exec gunicorn -b :5000 --access-logfile - --error-logfile - manage:app
+exec gunicorn -b :5000 --access-logfile - --error-logfile - --worker-class eventlet -w 1 manage:app
