6226ab0270dc75e97514857b0ce91808748d7c18
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2024-04-25 19:33:29 +0300

    concat envs in crontab

--- boot.sh
+++ boot.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-printenv | sed 's/^\(.*\)$/\1/g' > .cronenvs
+printenv | sed 's/^\(.*\)$/\1/g' > loaded.env
 flask db upgrade
 service cron start
 crontab explinks-crontab
+crontab -l >> loaded.env
+crontab loaded.env
 exec gunicorn -b :5000 --access-logfile - --error-logfile - --worker-class eventlet -w 1 manage:app
